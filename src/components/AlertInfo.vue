e9514e7393247aae71f261d3cb18d68023cd6065
igorjohn <89204391+igorjohn@users.noreply.github.com>
2022-12-19 12:57:20 -0300

    Alerts added

---
+++ src/components/AlertInfo.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="p-10 flex flex-col space-y-3">
+        <div class="bg-green-100 p-5 w-full sm:w-1/2 lg:w-1/4 rounded-md">
+            <div class="flex justify-between">
+                <div class="flex space-x-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="flex-none fill-current text-green-500 h-4 w-4">
+                        <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.25 16.518l-4.5-4.319 1.396-1.435 3.078 2.937 6.105-6.218 1.421 1.409-7.5 7.626z" />
+                    </svg>
+                    <div class="flex-1 leading-tight text-sm text-green-700 font-medium">Photo has been updated</div>
+                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="flex-none fill-current text-green-600 h-3 w-3">
+                    <path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z" />
+                </svg>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script type="text/javascript">
+export default {
+    name: "AlertInfo",
+};
+</script>
