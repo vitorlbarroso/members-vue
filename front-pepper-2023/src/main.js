9c7162ce019803c4a66d3fd88146d4416f79872f
vitorlbarroso <76413552+vitorlbarroso@users.noreply.github.com>
2022-12-19 15:09:15 -0300

    Created system structure

--- front-pepper-2023/src/main.js
+++ front-pepper-2023/src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueRouter from 'vue-router'
+import VueAxios from 'vue-axios';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+app.use(router);
+app.use(VueRouter);
+app.use(VueAxios);
+app.mount('#app');
--- front-pepper-2023/src/router/index.js
+++ front-pepper-2023/src/router/index.js
@@ -1,25 +1,102 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import LoginView from '../views/Login.vue';
+import ForgotPasswordView from '../views/ForgotPassword.vue';
+import MemberAreasView from '../views/MemberAreas/Index.vue';
+import MemberAreaView from '../views/MemberAreas/Area/Index.vue';
+import ProfileView from '../views/MemberAreas/Area/pages/Profile.vue';
+import ProductsView from '../views/MemberAreas/Area/pages/Products.vue';
+import ContentsView from '../views/MemberAreas/Area/pages/Contents.vue';
+import SettingsView from '../views/MemberAreas/Area/pages/Settings.vue';
+import ContactView from '../views/MemberAreas/Area/pages/Contact.vue';
+import ClassroomView from '../views/MemberAreas/Area/pages/Classroom.vue';
+import CertificatesView from '../views/MemberAreas/Area/pages/Certificates.vue';
+import CreateCertificateView from '../views/MemberAreas/Area/pages/CreateCertificate.vue';
+import ManageStudentsView from '../views/MemberAreas/Area/pages/ManageStudents.vue';
+import IntegrationsView from '../views/MemberAreas/Area/pages/Integrations.vue';
+import IntegrationPepperView from '../views/MemberAreas/Area/pages/integrations/Pepper.vue';
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/login',
+    name: 'Login',
+    component: LoginView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/forgot-password',
+    name: 'Forgot Password',
+    component: ForgotPasswordView
+  },
+  {
+    path: '/:catchAll(.*)', redirect:'Login'
+  },
+  {
+    path: '/memberareas',
+    name: 'MemberAreas',
+    component: MemberAreasView
+  },
+  {
+    path: '/:area',
+    component: MemberAreaView,
+    children: [
+      {
+        path: '/:area',
+        component: ProductsView
+      },
+      {
+        path: '/:area/:produto',
+        component: ContentsView
+      },
+      {
+        path: '/:area/:produto/:modulo/:conteudo',
+        component: ClassroomView
+      },
+      {
+        path: '/:area/certificados',
+        component: CertificatesView
+      },
+      {
+        path: '/:area/criar-certificado',
+        component: CreateCertificateView
+      },
+      {
+        path: '/:area/perfil',
+        component: ProfileView
+      },
+      {
+        path: '/:area/contato',
+        component: ContactView
+      },
+      {
+        path: '/:area/alunos',
+        component: ManageStudentsView
+      },
+      {
+        path: '/:area/integracoes',
+        component: IntegrationsView
+      },
+      {
+        path: '/:area/integracoes/pepper',
+        component: IntegrationPepperView
+      },
+      {
+        path: '/:area/configuracoes',
+        component: SettingsView
+      },
+    ]
+  },
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+router.beforeEach(async (to) => {
+  if (to.name == 'Login') {
+    if (localStorage.getItem('token')) {
+      return { name: 'MemberAreas' }
+    }
+  }
+});
 
 export default router
